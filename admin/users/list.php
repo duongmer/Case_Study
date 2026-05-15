f7449b4edd045b472ad2af61e4abcc9186c46f4b
duongmer <144136392+duongmer@users.noreply.github.com>
2026-05-15 19:44:31 +0700

    admin checkc

--- admin/users/list.php
+++ admin/users/list.php
@@ -1,33 +1,441 @@
 <?php
-$result = $conn->query("SELECT * FROM user");
+include("../connect.php");
+  $sql = "SELECT * FROM user ";
+  $result = mysqli_query($connect, $sql);
 ?>
 
-<h3>Danh sách người dùng</h3>
 
-<a href="index.php?page=users_add">+ Thêm</a>
 
-<table border="1">
-<tr>
-<th>ID</th>
-<th>Name</th>
-<th>Username</th>
-<th>Email</th>
-<th>Phone</th>
-<th>Hành động</th>
-</tr>
+<body onload="time()" class="app sidebar-mini rtl">
+  <!-- Navbar-->
+  <header class="app-header">
+    <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar"
+      aria-label="Hide Sidebar"></a>
+    <!-- Navbar Right Menu-->
+    <ul class="app-nav">
 
-<?php while($u = $result->fetch_assoc()): ?>
-<tr>
-<td><?= $u['ID'] ?></td>
-<td><?= $u['Name'] ?></td>
-<td><?= $u['Username'] ?></td>
-<td><?= $u['Email'] ?></td>
-<td><?= $u['Phone'] ?></td>
 
-<td>
-<a href="index.php?page=users_edit&id=<?= $u['ID'] ?>">Sửa</a> |
-<a href="users/delete.php?id=<?= $u['ID'] ?>">Xóa</a>
-</td>
-</tr>
-<?php endwhile; ?>
+      <!-- User Menu-->
+      <li><a class="app-nav__item" href="/index.html"><i class='bx bx-log-out bx-rotate-180'></i> </a>
+
+      </li>
+    </ul>
+  </header>
+  <!-- Sidebar menu-->
+    <?php
+        include('../menu.php');
+    ?>
+  <main class="app-content">
+    <div class="app-title">
+      <ul class="app-breadcrumb breadcrumb side">
+        <li class="breadcrumb-item active"><a href="#"><b>Danh sách nhân viên</b></a></li>
+      </ul>
+      <div id="clock"></div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="tile">
+          <div class="tile-body">
+
+            <div class="row element-button">
+              <div class="col-sm-2">
+
+                <a class="btn btn-add btn-sm" href="form-add-nhan-vien.html" title="Thêm"><i class="fas fa-plus"></i>
+                  Tạo mới nhân viên</a>
+              </div>
+              <div class="col-sm-2">
+                <a class="btn btn-delete btn-sm nhap-tu-file" type="button" title="Nhập" onclick="myFunction(this)"><i
+                    class="fas fa-file-upload"></i> Tải từ file</a>
+              </div>
+
+              <div class="col-sm-2">
+                <a class="btn btn-delete btn-sm print-file" type="button" title="In" onclick="myApp.printTable()"><i
+                    class="fas fa-print"></i> In dữ liệu</a>
+              </div>
+              <div class="col-sm-2">
+                <a class="btn btn-delete btn-sm print-file js-textareacopybtn" type="button" title="Sao chép"><i
+                    class="fas fa-copy"></i> Sao chép</a>
+              </div>
+
+              <div class="col-sm-2">
+                <a class="btn btn-excel btn-sm" href="" title="In"><i class="fas fa-file-excel"></i> Xuất Excel</a>
+              </div>
+              <div class="col-sm-2">
+                <a class="btn btn-delete btn-sm pdf-file" type="button" title="In" onclick="myFunction(this)"><i
+                    class="fas fa-file-pdf"></i> Xuất PDF</a>
+              </div>
+              <div class="col-sm-2">
+                <a class="btn btn-delete btn-sm" type="button" title="Xóa" onclick="myFunction(this)"><i
+                    class="fas fa-trash-alt"></i> Xóa tất cả </a>
+              </div>
+            </div>
+            <table class="table table-hover table-bordered js-copytextarea" cellpadding="0" cellspacing="0" border="0"
+              id="sampleTable">
+              <thead>
+                <tr>
+                  <th width="10"><input type="checkbox" id="all"></th>
+                  <th>ID nhân viên</th>
+                  <th width="150">Họ và tên</th>
+                  <th width="20">Ảnh thẻ</th>
+                  <th width="300">Địa chỉ</th>
+                  <th>Ngày sinh</th>
+                  <th>Giới tính</th>
+                  <th>SĐT</th>
+                  <th>Chức vụ</th>
+                  <th width="100">Tính năng</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td width="10"><input type="checkbox" name="check1" value="1"></td>
+                  <td>#CD12837</td>
+                  <td>Hồ Thị Thanh Ngân</td>
+                  <td><img class="img-card-person" src="/img-anhthe/1.jpg" alt=""></td>
+                  <td>155-157 Trần Quốc Thảo, Quận 3, Hồ Chí Minh </td>
+                  <td>12/02/1999</td>
+                  <td>Nữ</td>
+                  <td>0926737168</td>
+                  <td>Bán hàng</td>
+                  <td class="table-td-center"><button class="btn btn-primary btn-sm trash" type="button" title="Xóa"
+                      onclick="myFunction(this)"><i class="fas fa-trash-alt"></i>
+                    </button>
+                    <button class="btn btn-primary btn-sm edit" type="button" title="Sửa" id="show-emp"
+                      data-toggle="modal" data-target="#ModalUP"><i class="fas fa-edit"></i>
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td width="10"><input type="checkbox" name="check2" value="2"></td>
+                  <td>#SX22837</td>
+                  <td>Trần Khả Ái</td>
+                  <td><img class="img-card-person" src="/img-anhthe/2.jpg" alt=""></td>
+                  <td>6 Nguyễn Lương Bằng, Tân Phú, Quận 7, Hồ Chí Minh</td>
+                  <td>22/12/1999</td>
+                  <td>Nữ</td>
+                  <td>0931342432</td>
+                  <td>Bán hàng</td>
+                  <td><button class="btn btn-primary btn-sm trash" type="button" title="Xóa"
+                      onclick="myFunction(this)"><i class="fas fa-trash-alt"></i>
+                    </button>
+                    <button class="btn btn-primary btn-sm edit" type="button" title="Sửa" id="show-emp"
+                      data-toggle="modal" data-target="#ModalUP"><i class="fas fa-edit"></i>
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td width="10"><input type="checkbox" name="check3" value="3"></td>
+                  <td>#LO2871</td>
+                  <td>Phạm Thu Cúc</td>
+                  <td><img class="img-card-person" src="/img-anhthe/3.jpg" alt=""></td>
+                  <td>Số 3 Hòa Bình, Phường 3, Quận 11, Hồ Chí Minh </td>
+                  <td>02/06/1998</td>
+                  <td>Nữ</td>
+                  <td>0931491997</td>
+                  <td>Thu ngân</td>
+                  <td><button class="btn btn-primary btn-sm trash" type="button" title="Xóa" onclick="myFunction()"><i
+                        class="fas fa-trash-alt"></i>
+                    </button>
+                    <button class="btn btn-primary btn-sm edit" type="button" title="Sửa" id="show-emp"
+                      data-toggle="modal" data-target="#ModalUP"><i class="fas fa-edit"></i>
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td width="10"><input type="checkbox"></td>
+                  <td>#SR28746</td>
+                  <td>Trần Anh Khoa</td>
+                  <td><img class="img-card-person" src="/img-anhthe/4.jpg" alt=""></td>
+                  <td>19 Đường Nguyễn Hữu Thọ, Tân Hưng, Quận 7, Hồ Chí Minh </td>
+                  <td>18/02/1995</td>
+                  <td>Nam</td>
+                  <td>0916706633</td>
+                  <td>Tư vấn</td>
+                  <td><button class="btn btn-primary btn-sm trash" type="button" title="Xóa" onclick="myFunction()"><i
+                        class="fas fa-trash-alt"></i>
+                    </button>
+                    <button class="btn btn-primary btn-sm edit" type="button" title="Sửa" id="show-emp"
+                      data-toggle="modal" data-target="#ModalUP"><i class="fas fa-edit"></i>
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td width="10"><input type="checkbox"></td>
+                  <td>#KJS276</td>
+                  <td>Nguyễn Thành Nhân</td>
+                  <td><img class="img-card-person" src="/img-anhthe/5.jpg" alt=""></td>
+                  <td>Số 13, Tân Thuận Đông, Quận 7, Hồ Chí Minh </td>
+                  <td>10/03/1996</td>
+                  <td>Nam</td>
+                  <td>0971038066</td>
+                  <td>Bảo trì</td>
+                  <td><button class="btn btn-primary btn-sm trash" type="button" title="Xóa" onclick="myFunction()"><i
+                        class="fas fa-trash-alt"></i>
+                    </button>
+                    <button class="btn btn-primary btn-sm edit" type="button" title="Sửa" id="show-emp"
+                      data-toggle="modal" data-target="#ModalUP"><i class="fas fa-edit"></i>
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td width="10"><input type="checkbox"></td>
+                  <td>#BS76228</td>
+                  <td>Nguyễn Đặng Trọng Nhân</td>
+                  <td><img class="img-card-person" src="/img-anhthe/6.jpg" alt=""></td>
+                  <td>59C Nguyễn Đình Chiểu, Quận 3, Hồ Chí Minh </td>
+                  <td>23/07/1996</td>
+                  <td>Nam</td>
+                  <td>0846881155</td>
+                  <td>Dịch vụ</td>
+                  <td><button class="btn btn-primary btn-sm trash" type="button" title="Xóa" onclick="myFunction()"><i
+                        class="fas fa-trash-alt"></i>
+                    </button>
+                    <button class="btn btn-primary btn-sm edit" type="button" title="Sửa" id="show-emp"
+                      data-toggle="modal" data-target="#ModalUP"><i class="fas fa-edit"></i>
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td width="10"><input type="checkbox"></td>
+                  <td>#YUI21376</td>
+                  <td>Nguyễn Thị Mai</td>
+                  <td><img class="img-card-person" src="/img-anhthe/4.jpg" alt=""></td>
+                  <td>Đường Số 3, Tân Tạo A, Bình Tân, Hồ Chí Minh</td>
+                  <td>09/12/2000</td>
+                  <td>Nữ </td>
+                  <td>0836333037</td>
+                  <td>Tư vấn</td>
+                  <td><button class="btn btn-primary btn-sm trash" title="Xóa" onclick="myFunction()"><i
+                        class="fas fa-trash-alt"></i>
+                    </button>
+                    <button class="btn btn-primary btn-sm edit" title="Sửa" id="show-emp" data-toggle="modal"
+                      data-target="#ModalUP"><i class="fas fa-edit"></i>
+                    </button>
+                  </td>
+                </tr>
+
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <!--
+  MODAL
+-->
+  <div class="modal fade" id="ModalUP" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static"
+    data-keyboard="false">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+
+        <div class="modal-body">
+          <div class="row">
+            <div class="form-group  col-md-12">
+              <span class="thong-tin-thanh-toan">
+                <h5>Chỉnh sửa thông tin nhân viên cơ bản</h5>
+              </span>
+            </div>
+          </div>
+          <table class="table table-hover table-bordered js-copytextarea" cellpadding="0" cellspacing="0" border="0"
+    id="sampleTable">
+
+            <thead>
+                <tr>
+                    <th width="10"><input type="checkbox" id="all"></th>
+                    <th>ID</th>
+                    <th>Họ và tên</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Role</th>
+                    <th>Phone</th>
+                    <th>Avatar</th>
+                    <th width="100">Tính năng</th>
+                </tr>
+            </thead>
+
+            <tbody>
+        <?php while($row = mysqli_fetch_assoc($result)) { ?>
+        <tr>
+            <td><input type="checkbox"></td>
+
+            <td><?php echo $row['id']; ?></td>
+            <td><?php echo $row['name']; ?></td>
+
+            <td>
+                <img src="../../images/<?php echo $row['avatar']; ?>" width="70">
+            </td>
+
+            <td><?php echo $row['address']; ?></td>
+            <td><?php echo $row['birthday']; ?></td>
+            <td><?php echo $row['gender']; ?></td>
+            <td><?php echo $row['phone']; ?></td>
+            <td><?php echo $row['role']; ?></td>
+
+            <td>
+                <a href="edit.php?id=<?php echo $row['id']; ?>">
+                    <button>Sửa</button>
+                </a>
+
+                <a href="delete.php?id=<?php echo $row['id']; ?>">
+                    <button>Xóa</button>
+                </a>
+            </td>
+        </tr>
+        <?php } ?>
+        </tbody>
+
 </table>
+
+          <BR>
+          <a href="#" style="    float: right;
+        font-weight: 600;
+        color: #ea0000;">Chỉnh sửa nâng cao</a>
+          <BR>
+          <BR>
+          <button class="btn btn-save" type="button">Lưu lại</button>
+          <a class="btn btn-cancel" data-dismiss="modal" href="#">Hủy bỏ</a>
+          <BR>
+        </div>
+        <div class="modal-footer">
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--
+  MODAL
+-->
+
+  <!-- Essential javascripts for application to work-->
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="src/jquery.table2excel.js"></script>
+  <script src="js/main.js"></script>
+  <!-- The javascript plugin to display page loading on top-->
+  <script src="js/plugins/pace.min.js"></script>
+  <!-- Page specific javascripts-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
+  <!-- Data table plugin-->
+  <script type="text/javascript" src="js/plugins/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" src="js/plugins/dataTables.bootstrap.min.js"></script>
+  <script type="text/javascript">$('#sampleTable').DataTable();</script>
+  <script>
+    function deleteRow(r) {
+      var i = r.parentNode.parentNode.rowIndex;
+      document.getElementById("myTable").deleteRow(i);
+    }
+    jQuery(function () {
+      jQuery(".trash").click(function () {
+        swal({
+          title: "Cảnh báo",
+         
+          text: "Bạn có chắc chắn là muốn xóa nhân viên này?",
+          buttons: ["Hủy bỏ", "Đồng ý"],
+        })
+          .then((willDelete) => {
+            if (willDelete) {
+              swal("Đã xóa thành công.!", {
+                
+              });
+            }
+          });
+      });
+    });
+    oTable = $('#sampleTable').dataTable();
+    $('#all').click(function (e) {
+      $('#sampleTable tbody :checkbox').prop('checked', $(this).is(':checked'));
+      e.stopImmediatePropagation();
+    });
+
+    //EXCEL
+    // $(document).ready(function () {
+    //   $('#').DataTable({
+
+    //     dom: 'Bfrtip',
+    //     "buttons": [
+    //       'excel'
+    //     ]
+    //   });
+    // });
+
+
+    //Thời Gian
+    function time() {
+      var today = new Date();
+      var weekday = new Array(7);
+      weekday[0] = "Chủ Nhật";
+      weekday[1] = "Thứ Hai";
+      weekday[2] = "Thứ Ba";
+      weekday[3] = "Thứ Tư";
+      weekday[4] = "Thứ Năm";
+      weekday[5] = "Thứ Sáu";
+      weekday[6] = "Thứ Bảy";
+      var day = weekday[today.getDay()];
+      var dd = today.getDate();
+      var mm = today.getMonth() + 1;
+      var yyyy = today.getFullYear();
+      var h = today.getHours();
+      var m = today.getMinutes();
+      var s = today.getSeconds();
+      m = checkTime(m);
+      s = checkTime(s);
+      nowTime = h + " giờ " + m + " phút " + s + " giây";
+      if (dd < 10) {
+        dd = '0' + dd
+      }
+      if (mm < 10) {
+        mm = '0' + mm
+      }
+      today = day + ', ' + dd + '/' + mm + '/' + yyyy;
+      tmp = '<span class="date"> ' + today + ' - ' + nowTime +
+        '</span>';
+      document.getElementById("clock").innerHTML = tmp;
+      clocktime = setTimeout("time()", "1000", "Javascript");
+
+      function checkTime(i) {
+        if (i < 10) {
+          i = "0" + i;
+        }
+        return i;
+      }
+    }
+    //In dữ liệu
+    var myApp = new function () {
+      this.printTable = function () {
+        var tab = document.getElementById('sampleTable');
+        var win = window.open('', '', 'height=700,width=700');
+        win.document.write(tab.outerHTML);
+        win.document.close();
+        win.print();
+      }
+    }
+    //     //Sao chép dữ liệu
+    //     var copyTextareaBtn = document.querySelector('.js-textareacopybtn');
+
+    // copyTextareaBtn.addEventListener('click', function(event) {
+    //   var copyTextarea = document.querySelector('.js-copytextarea');
+    //   copyTextarea.focus();
+    //   copyTextarea.select();
+
+    //   try {
+    //     var successful = document.execCommand('copy');
+    //     var msg = successful ? 'successful' : 'unsuccessful';
+    //     console.log('Copying text command was ' + msg);
+    //   } catch (err) {
+    //     console.log('Oops, unable to copy');
+    //   }
+    // });
+
+
+    //Modal
+    $("#show-emp").on("click", function () {
+      $("#ModalUP").modal({ backdrop: false, keyboard: false })
+    });
+  </script>
+</body>
+
+</html>
--- connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root', '', 'gtpt');
-if($conn->connect_error){
-    die('Kết nối thất bại: ' . $conn->connect_error);
-}
-?>
